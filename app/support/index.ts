4d129f298c7c6f6a169e8e633f231c071d5ad194
Jacob <harel1990@gmail.com>
2026-08-23 23:50:55 -0300

    feat: export support staging module

---
+++ app/support/index.ts
@@ -0,0 +1,6 @@
+export * from "./types.js";
+export * from "./config.js";
+export * from "./threading.js";
+export * from "./classifier.js";
+export * from "./imap-contract.js";
+export * from "./sync.js";
